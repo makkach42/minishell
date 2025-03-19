d1ff2055cd52ab943119298aa2912f903933e7ac
makkach <makkach@e3r6p8.1337.ma>
2025-03-19 12:48:45 +0000

    minishell_parsing

--- minishell.h
+++ minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 12:46:34 by makkach           #+#    #+#             */
+/*   Updated: 2025/03/19 12:47:30 by makkach          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -9,17 +21,17 @@
 
 typedef struct s_list
 {
-    char *data;
-    char *token;
-    struct s_list *next;
-}   t_list;
+	char			*data;
+	char			*token;
+	struct s_list	*next;
+}	t_list;
 
 char	*ft_strdup(char *s1);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strtrim(char *s1, char *set);
 char	**ft_split(char *s, char c);
 size_t	ft_strlen(char *str);
-char	*ft_substr(char  *s, unsigned int start, size_t len);
-int    ft_strcmp(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
