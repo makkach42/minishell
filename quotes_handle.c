70cec6e62b861c0d7234f85fb7f280a14850d649
makkach <makkach@e3r6p8.1337.ma>
2025-04-25 15:20:19 +0100

    minishell_parsing

--- quotes_handle.c
+++ quotes_handle.c
@@ -6,34 +6,20 @@
 /*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 13:52:23 by makkach           #+#    #+#             */
-/*   Updated: 2025/04/24 17:06:15 by makkach          ###   ########.fr       */
+/*   Updated: 2025/04/25 15:19:54 by makkach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	quote_remove_two(t_tree **tree)
+int	count_filtered_length(char *old_str)
 {
-	int		i;
-	int		j;
-	int		k;
-	char	*old_str;
-	char	*new_str;
-	int		len;
-	int		final_len;
-	int		in_single_quotes;
-	int		in_double_quotes;
+	int	i;
+	int	len;
+	int	final_len;
+	int	in_single_quotes;
+	int	in_double_quotes;
 
-	if ((*tree)->left)
-		quote_remove_two(&(*tree)->left);
-	if ((*tree)->right)
-		quote_remove_two(&(*tree)->right);
-	if ((*tree)->command_arr)
-	{
-		k = 0;
-		while ((*tree)->command_arr[k])
-		{
-			old_str = (*tree)->command_arr[k];
 			len = ft_strlen(old_str);
 			final_len = 0;
 			in_single_quotes = 0;
@@ -49,17 +35,33 @@ void	quote_remove_two(t_tree **tree)
 					final_len++;
 				i++;
 			}
+	return (final_len);
+}
+
+void	create_filtered_string_inits(int *j, int *in_single_quotes,
+		int *in_double_quotes, int *i)
+{
+	*j = 0;
+	*in_single_quotes = 0;
+	*in_double_quotes = 0;
+	*i = 0;
+}
+
+char	*create_filtered_string(char *old_str, int final_len)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	*new_str;
+	int		in_single_quotes;
+	int		in_double_quotes;
+
 			new_str = (char *)t_malloc(sizeof(char) * (
 						final_len + 1), 2341, "parsing.c");
 			if (!new_str)
-			{
-				k++;
-				continue ;
-			}
-			j = 0;
-			in_single_quotes = 0;
-			in_double_quotes = 0;
-			i = 0;
+		return (NULL);
+	len = ft_strlen(old_str);
+	create_filtered_string_inits(&j, &in_single_quotes, &in_double_quotes, &i);
 			while (i < len)
 			{
 				if (old_str[i] == '\'' && !in_double_quotes)
@@ -67,16 +69,46 @@ void	quote_remove_two(t_tree **tree)
 				else if (old_str[i] == '\"' && !in_single_quotes)
 					in_double_quotes = !in_double_quotes;
 				else
-				{
-					new_str[j] = old_str[i];
-					j++;
-				}
+			new_str[j++] = old_str[i];
 				i++;
 			}
 			new_str[j] = '\0';
+	return (new_str);
+}
+
+void	process_command_string(t_tree **tree, int k)
+{
+	char	*old_str;
+	char	*new_str;
+	int		final_len;
+
+	old_str = (*tree)->command_arr[k];
+	final_len = count_filtered_length(old_str);
+	new_str = create_filtered_string(old_str, final_len);
+	if (!new_str)
+		return ;
 			t_free((*tree)->command_arr[k], 2370, "parsing.c");
 			(*tree)->command_arr[k] = new_str;
+}
+
+void	process_command_array(t_tree **tree)
+{
+	int	k;
+
+	k = 0;
+	while ((*tree)->command_arr[k])
+	{
+		process_command_string(tree, k);
 			k++;
 		}
 	}
+
+void	quote_remove_two(t_tree **tree)
+{
+	if ((*tree)->left)
+		quote_remove_two(&(*tree)->left);
+	if ((*tree)->right)
+		quote_remove_two(&(*tree)->right);
+	if ((*tree)->command_arr)
+		process_command_array(tree);
 }
