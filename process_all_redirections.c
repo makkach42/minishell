718425e0e41e35059dfaa17345ea9c74ab4d9f33
makkach <makkach@e3r6p8.1337.ma>
2025-04-26 18:51:03 +0100

    minishell_parsing

--- process_all_redirections.c
+++ process_all_redirections.c
@@ -6,7 +6,7 @@
 /*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 13:43:29 by makkach           #+#    #+#             */
-/*   Updated: 2025/04/26 18:43:01 by makkach          ###   ########.fr       */
+/*   Updated: 2025/04/26 18:49:56 by makkach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,116 +67,123 @@ int	check_if_in_string(char *cmd)
 	return (1);
 }
 
-void	extract_redirections(char *cmd_str, char **cmd_part, char **redir_part)
+int is_command_char(char c, int in_quotes)
 {
-	int		i;
-	int		j;
-	int		cmd_len;
-	int		in_quotes;
-	int		paren_count;
-	char	quote_type;
-	char	*redir_start;
-	char	*args_start;
-	int		redir_active;
-	int		redir_len;
-	char	*tmp;
+    return (c != '>' && c != '<') || in_quotes;
+}
 
-	*cmd_part = NULL;
-	*redir_part = NULL;
-	if (!cmd_str)
-		return ;
-	i = 0;
-	in_quotes = 0;
-	paren_count = 0;
-	quote_type = 0;
-	redir_start = NULL;
-	while (cmd_str[i])
-	{
-		if ((cmd_str[i] == '\'' || cmd_str[i] == '\"') && !in_quotes)
-		{
-			in_quotes = 1;
-			quote_type = cmd_str[i];
-		}
-		else if (in_quotes && cmd_str[i] == quote_type)
-			in_quotes = 0;
-		if (!in_quotes)
-		{
-			if (cmd_str[i] == '(')
-				paren_count++;
-			else if (cmd_str[i] == ')')
-				paren_count--;
-			if ((cmd_str[i] == '>' || cmd_str[i] == '<')
-				&& paren_count == 0 && !redir_start)
-			{
-				redir_start = &cmd_str[i];
-				break ;
-			}
-		}
-		i++;
-	}
-	if (!redir_start)
-	{
-		*cmd_part = ft_strdup(cmd_str);
-		return ;
-	}
-	cmd_len = redir_start - cmd_str;
-	*cmd_part = ft_substr_leak(cmd_str, 0, cmd_len, __LINE__);
-	args_start = NULL;
-	i = 0;
-	in_quotes = 0;
-	quote_type = 0;
-	redir_active = 0;
-	while (redir_start[i])
-	{
-		if ((redir_start[i] == '\'' || redir_start[i] == '\"') && !in_quotes)
-		{
-			in_quotes = 1;
-			quote_type = redir_start[i];
-		}
-		else if (in_quotes && redir_start[i] == quote_type)
-			in_quotes = 0;
-		if (!in_quotes)
-		{
-			if (redir_start[i] == '>' || redir_start[i] == '<')
-				redir_active = 1;
-			else if ((redir_active && redir_start[i] == ' ') && (i > 0) && (
-					redir_start[i - 1] != '>') && (redir_start[i - 1] != '<'))
-			{
-				j = i + 1;
-				while (redir_start[j] && redir_start[j] == ' ')
-					j++;
-				if (redir_start[j] && (
-						redir_start[j] != '>') && redir_start[j] != '<')
-				{
-					args_start = &redir_start[j];
-					break ;
-				}
-				else if (redir_start[j] == '>' || redir_start[j] == '<')
-					i = j - 1;
-				else
-					break ;
-			}
-			else if (redir_active && redir_start[i] != ' ')
-				redir_active = 2;
-		}
-		i++;
-	}
-	if (args_start)
-	{
-		redir_len = args_start - redir_start;
-		*redir_part = ft_substr_leak(redir_start, 0, redir_len, __LINE__);
-		tmp = *cmd_part;
-		if (tmp[0] != '\0')
-			*cmd_part = ft_strjoin_leak(tmp, " ", __LINE__);
-		else
-			*cmd_part = ft_strdup("");
-		t_free(tmp, __LINE__, "parsing.c");
-		tmp = *cmd_part;
-		*cmd_part = ft_strjoin_leak(tmp, args_start, __LINE__);
-		t_free(tmp, __LINE__, "parsing.c");
-	}
-	else
-		*redir_part = ft_strdup(redir_start);
+int skip_spaces_in_str(char *str, int start)
+{
+    while (str[start] && str[start] == ' ')
+        start++;
+    return start;
+}
+
+void extract_redirections(char *cmd_str, char **cmd_part, char **redir_part)
+{
+    int     i;
+    int     in_quotes;
+    char    quote_type;
+    char    *command_buf;
+    char    *redir_buf;
+    int     cmd_pos;
+    int     redir_pos;
+    int     paren_count;
+
+    *cmd_part = NULL;
+    *redir_part = NULL;
+    if (!cmd_str)
+        return;
+
+    command_buf = malloc(ft_strlen(cmd_str) + 1);
+    redir_buf = malloc(ft_strlen(cmd_str) + 1);
+    if (!command_buf || !redir_buf)
+    {
+        if (command_buf)
+            t_free(command_buf, __LINE__, "parsing.c");
+        if (redir_buf)
+            t_free(redir_buf, __LINE__, "parsing.c");
+        return;
+    }
+    i = 0;
+    cmd_pos = 0;
+    redir_pos = 0;
+    in_quotes = 0;
+    paren_count = 0;
+    quote_type = 0;
+    while (cmd_str[i])
+    {
+        if ((cmd_str[i] == '\'' || cmd_str[i] == '\"') && !in_quotes)
+        {
+            in_quotes = 1;
+            quote_type = cmd_str[i];
+        }
+        else if (in_quotes && cmd_str[i] == quote_type)
+            in_quotes = 0;
+        if (!in_quotes)
+        {
+            if (cmd_str[i] == '(')
+                paren_count++;
+            else if (cmd_str[i] == ')')
+                paren_count--;
+        }
+        if ((cmd_str[i] == '>' || cmd_str[i] == '<') && !in_quotes && paren_count == 0)
+        {
+            redir_buf[redir_pos++] = cmd_str[i];
+            if (cmd_str[i+1] && cmd_str[i] == cmd_str[i+1])
+            {
+                redir_buf[redir_pos++] = cmd_str[++i];
+            }
+            redir_buf[redir_pos++] = ' ';
+            i++;
+            while (cmd_str[i] && cmd_str[i] == ' ')
+                i++;
+            while (cmd_str[i] && cmd_str[i] != ' ' && cmd_str[i] != '>' && cmd_str[i] != '<')
+            {
+                if ((cmd_str[i] == '\'' || cmd_str[i] == '\"'))
+                {
+                    char file_quote = cmd_str[i++];
+                    redir_buf[redir_pos++] = file_quote;
+                    
+                    while (cmd_str[i] && cmd_str[i] != file_quote)
+                        redir_buf[redir_pos++] = cmd_str[i++];
+                    
+                    if (cmd_str[i])
+                        redir_buf[redir_pos++] = cmd_str[i];
+                }
+                else
+                {
+                    redir_buf[redir_pos++] = cmd_str[i];
+                }
+                i++;
+            }
+            redir_buf[redir_pos++] = ' ';
+            i--;
+        }
+        else if (cmd_str[i] == ' ')
+        {
+            command_buf[cmd_pos++] = cmd_str[i];
+        }
+        else if (is_command_char(cmd_str[i], in_quotes || paren_count > 0))
+        {
+            command_buf[cmd_pos++] = cmd_str[i];
+        }
+        i++;
+    }
+    command_buf[cmd_pos] = '\0';
+    redir_buf[redir_pos] = '\0';
+    *cmd_part = ft_strtrim(command_buf, " ");
+    t_free(command_buf, __LINE__, "parsing.c");
+    
+    if (redir_pos > 0)
+    {
+        *redir_part = ft_strtrim(redir_buf, " ");
+    }
+    else
+    {
+        *redir_part = ft_strdup("");
+    }
+    t_free(redir_buf, __LINE__, "parsing.c");
 }
 
 void	process_all_redirections_helper(t_tree **tree, char **cmd)
