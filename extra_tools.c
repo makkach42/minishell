0dfcf0c9998c6d9186017420578978549354f7c5
makkach <makkach@e3r6p8.1337.ma>
2025-04-27 14:54:48 +0100

    minishell_parsing

--- extra_tools.c
+++ extra_tools.c
@@ -6,7 +6,7 @@
 /*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 14:28:43 by makkach           #+#    #+#             */
-/*   Updated: 2025/04/27 14:49:51 by makkach          ###   ########.fr       */
+/*   Updated: 2025/04/27 14:54:24 by makkach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,69 @@
 
 void	print_tree_visual(t_tree *tree, int level, int is_left)
 {
+	int	indent_index;
+	int	arr_index;
+	int	marker_index;
+
 	if (!tree)
 		return ;
-	for (int i = 0; i < level; i++)
+	indent_index = 0;
+	while (indent_index < level)
 	{
-		if (i == level - 1)
-			printf("%s", is_left ? "----" : "    ");
+		if (indent_index == level - 1)
+		{
+			if (is_left)
+				printf("----");
+			else
+				printf("    ");
+		}
 		else
 			printf("    ");
+		indent_index++;
 	}
-	printf("%s", tree->type ? tree->type : "NULL");
-	if (tree->command)
-	{
-		printf(" (Command: %s)", tree->command);
-	}
+	if (tree->type)
+		printf("%s", tree->type);
 	else
-	{
+		printf("NULL");
+	if (tree->command)
+		printf(" (Command: %s)", tree->command);
+	else
 		printf(" (Command: NULL)");
-	}
 	if (tree->command_arr)
 	{
-		for (int i = 0; tree->command_arr[i]; i++)
+		arr_index = 0;
+		while (tree->command_arr[arr_index])
 		{
-			printf(" (Command_arr: %s)", tree->command_arr[i]);
+			printf(" (Command_arr: %s)", tree->command_arr[arr_index]);
+			arr_index++;
 		}
 	}
 	else
-	{
 		printf(" (Command_arr: NULL)");
-	}
 	if (tree->redirections)
-	{
 		printf(" [Redirections: %s]", tree->redirections);
-	}
-	printf("\n");
 	if (tree->left || tree->right)
 	{
-		for (int i = 0; i < level; i++)
+		marker_index = 0;
+		while (marker_index < level)
+		{
 			printf("    ");
+			marker_index++;
+		}
 		printf("---left;\n");
 	}
 	if (tree->left)
-	{
 		print_tree_visual(tree->left, level + 1, 1);
-	}
 	if (tree->right)
 	{
-		for (int i = 0; i < level; i++)
+		marker_index = 0;
+		while (marker_index < level)
+		{
 			printf("    ");
+			marker_index++;
+		}
 		printf("---right;\n");
 	}
 	if (tree->right)
-	{
 		print_tree_visual(tree->right, level + 1, 0);
-	}
 }
