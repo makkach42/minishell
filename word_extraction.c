7c3e4d82193109cd2c090d6dc8e3e906b7345667
makkach <makkach@e3r6p8.1337.ma>
2025-05-08 12:49:12 +0100

    minishell_parsing

--- word_extraction.c
+++ word_extraction.c
@@ -6,7 +6,7 @@
 /*   By: makkach <makkach@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 10:36:34 by makkach           #+#    #+#             */
-/*   Updated: 2025/05/04 11:12:13 by makkach          ###   ########.fr       */
+/*   Updated: 2025/05/08 12:48:25 by makkach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ int	operation_or_pipe_before_condition(char *str, int flag)
 	}
 	else if (flag == 2)
 	{
-		while (str[i] && (str[i] != '"' || str[i] != '\''))
+		while ((str[i] != '"' && str[i] != '\'') && str[i])
 		{
 			if (is_operator(str[i]))
 				break ;
